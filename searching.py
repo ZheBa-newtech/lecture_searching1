d6ed4dba4b9673226420c5d383facffc6db92f71
Shulika Yehor (271403) <271403@vutbr.cz>
2026-04-16 15:33:34 +0200

    tra lala

--- searching.py
+++ searching.py
@@ -3,29 +3,25 @@ import json
 
 
 def read_data(file_name, field):
-    """
-    Reads a JSON file and returns data for a given field.
+    file_name_js = file_name
+    with open(file_name_js, 'r') as js_file:
+        data = json.load(js_file)
+        data = dict(data)
 
-    Args:
-        file_name (str): Name of the JSON file.
-        field (str): Key to retrieve from the JSON data.
-            Must be one of: 'unordered_numbers', 'ordered_numbers' or 'dna_sequence'.
-
-    Returns:
-        list | str | None:
-            - list: If data retrieved by the selected field contains numeric data.
-            - str: If field is 'dna_sequence'.
-            - None: If the field is not supported.
-    """
-    # get current working directory path
-    cwd_path = Path.cwd()
-    
-    file_path = cwd_path / file_name
+    for dat in data:
+        if field in data:
+            return data[field]
+        else:
+            return None
 
 
-def main():
-    pass
 
 
-if __name__ == "__main__":
-    main()
+# cwd_path = Path.cwd()
+#file_path = cwd_path / file_name
+#def main():
+#pass
+#if __name__ == 'main':
+#main()
+
+print(read_data(file_name='sequential.json', field='unordered_numbers'))
